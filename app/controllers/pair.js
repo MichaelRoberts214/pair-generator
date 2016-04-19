bfbcab78c4b5764f11f60ae38a6689503c54a70f
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2016-04-19 09:51:44 -0700

    implement here toggle save action

--- app/controllers/pair.js
+++ app/controllers/pair.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    presentToggle: function (user) {
+      user.toggleProperty('here');
+      user.save().then(function () {
+        console.log('save success');
+      }, function (error) {
+        console.error(error);
+      });
+    }
+  }
 });
