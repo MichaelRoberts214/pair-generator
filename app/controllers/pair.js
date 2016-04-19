226ae16f99d91200b8351f6e9682cb2fcd500472
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2016-04-19 10:08:12 -0700

    placeholder generatePairs code

--- app/controllers/pair.js
+++ app/controllers/pair.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   pairs: Ember.A([]),
+  roster: Ember.computed.filterBy('model', 'here'),
 
   actions: {
     presentToggle: function (user) {
@@ -14,7 +15,46 @@ export default Ember.Controller.extend({
     },
 
     generatePairs: function () {
-      //
+      var pairs = [];
+      var n = roster.length;
+
+      while (n > 0) {
+        if (n === 1) {
+          pairs.push(roster[0]);
+          n = 0;
+        } else {
+          var ind = Math.round(Math.random() * (n - 1));
+          var ind2 = Math.round(Math.random() * (n - 1));
+          while (ind2 === ind) {
+            ind2 = Math.round(Math.random() * (n - 1));
+          }
+          var pairString = '';
+          pairString = pairString + roster.splice(ind, 1);
+          if (ind < ind2) {
+            let temp = ind2 - 1;
+            ind2 = temp >= n ? 0 : temp;
+          } else if (ind > ind2) {
+            ind2 = ind2 >= n ? 0 : ind2;
+          } else {
+            ind2 = 0;
+          }
+          pairString = pairString + ", " + roster.splice(ind2, 1);
+          pairs.push(pairString);
+          n = roster.length;
+        }
+      }
+
+      function makeUL(array) {
+        var list = document.createElement('ul');
+        for(var i = 0; i < array.length; i++) {
+          var item = document.createElement('li');
+          item.appendChild(document.createTextNode(array[i]));
+          list.appendChild(item);
+        }
+        return list;
+      }
+      document.getElementById('list').appendChild(makeUL(pairs));
+
     }
   }
 });
